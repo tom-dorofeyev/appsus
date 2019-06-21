09845834f576945391d28ecb876dc7375f55fff1
Nadav Abuhatsira <Nudu22@gmail.com>
2019-06-21 20:28:08 +0300

    New Mail now Pushes to DB

--- js/apps/email/services/email.service.js
+++ js/apps/email/services/email.service.js
@@ -14,13 +14,13 @@ let emailsDB = [
         to: 'mymail@savta.net',
         timestamp: 'Thu Jun 20 2019 11:48:49',
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quisquam consequuntur placeat, possimus animi unde aliquid natus assumenda, quia non iste magnam consectetur sequi beatae? Necessitatibus eum impedit accusantium. Sunt!',
-        type:{
+        type: {
             isStar: false,
             isTrash: false,
             isRead: true,
             isMultSelected: false,
         },
-        images:[{
+        images: [{
             profile: 'img/email-imgs/generic-profile.png'
         }]
     },
@@ -31,13 +31,13 @@ let emailsDB = [
         to: 'mymail@savta.net',
         timestamp: 'Thu Jun 19 2019 11:48:49',
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quisquam consequuntur placeat, possimus animi unde aliquid natus assumenda, quia non iste magnam consectetur sequi beatae? Necessitatibus eum impedit accusantium. Sunt!',
-        type:{
+        type: {
             isStar: false,
             isTrash: false,
             isRead: false,
             isMultSelected: false,
         },
-        images:[{
+        images: [{
             profile: 'img/email-imgs/generic-profile.png'
         }]
     },
@@ -48,55 +48,62 @@ let emailsDB = [
         to: 'mymail@savta.net',
         timestamp: 'Thu Jun 16 2019 11:48:49',
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quisquam consequuntur placeat, possimus animi unde aliquid natus assumenda, quia non iste magnam consectetur sequi beatae? Necessitatibus eum impedit accusantium. Sunt!',
-        type:{
+        type: {
             isStar: false,
             isTrash: false,
             isRead: false,
             isMultSelected: false,
         },
-        images:[{
+        images: [{
             profile: 'img/email-imgs/generic-profile.png'
         }]
     }
 ]
 
-changeEmail(0,'title', 'we love sawsan');
 
-function query(){
+function query() {
     let emails = storageService.load(EMAILS_KEY);
-    if(!emails){
+    if (!emails) {
         emails = emailsDB;
         storageService.store(EMAILS_KEY, emails);
     }
     return Promise.resolve(emails);
 }
 
-function getFilteredEmails(filterWord){
-    if(filterWord === 'all'){
+function getFilteredEmails(filterWord) {
+    if (filterWord === 'all') {
         return query();
-    } else if(filterWord === 'read'){
-        return query().then(emails=>emails.filter(email=>email.type.isRead));
-    } else if(filterWord ==='unread'){
-        return query().then(emails=>emails.filter(email=>!email.type.isRead));
-    } else{
-        return query().then(emails=>emails.filter(email=>email.title.includes(filterWord)));
+    } else if (filterWord === 'read') {
+        return query().then(emails => emails.filter(email => email.type.isRead));
+    } else if (filterWord === 'unread') {
+        return query().then(emails => emails.filter(email => !email.type.isRead));
+    } else {
+        return query().then(emails => emails.filter(email => email.title.includes(filterWord)));
     }
 }
 
-function getEmailById(id){
-    return query().then(emails=>{
-        return emails.find(email=>email.id === id);
+function getEmailById(id) {
+    return query().then(emails => {
+        return emails.find(email => email.id === id);
     })
 }
 
-function changeEmail(index, key, newValue){
-    query().then(emails=>emails[index][key] = newValue)
+function changeEmail(index, key, newValue) {
+    query().then(emails => emails[index][key] = newValue)
 }
 
-export default {
-    query,
-    getEmailById,
-    getFilteredEmails,
+function add(newEmail) {
+    emailsDB.unshift(newEmail)
+    let emails = emailsDB
+    storageService.store(EMAILS_KEY, emails)
 }
+
+    export default {
+        query,
+        getEmailById,
+        getFilteredEmails,
+        add,
+        emailsDB,
+    }
 
 
