c723b320900d45ebba32328675c3559ab0dc30d7
Tom Dorofeyev <[tomdorofey@gmail.com]>
2019-06-23 22:23:06 +0300

    moving pinned notes

--- js/apps/keep/cmps/note-list.cmp.js
+++ js/apps/keep/cmps/note-list.cmp.js
@@ -3,8 +3,13 @@ import singleNote from './single-note.cmp.js'
 export default {
     props: ['noteList'],
     template: `
-    <section class="note-list-container flex">
-        <single-note v-for="currNote in noteList" :key="currNote.id" :note="currNote"></single-note>
+    <section class="note-list-container flex column">
+        <div class="pinned-container flex">
+            <single-note v-for="currNote in noteList" v-if="currNote.isPinned" :key="currNote.id" :note="currNote"></single-note>
+        </div>
+        <div class="not-pinned-container flex">
+            <single-note v-for="currNote in noteList" v-if="!currNote.isPinned" :key="currNote.id" :note="currNote"></single-note>
+        </div>
     </section>
     `,
     components:{
