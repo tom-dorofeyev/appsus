21f3c65337a35ab876ce7252f7e05a2c9a6bae39
Nadav Abuhatsira <Nudu22@gmail.com>
2019-06-23 12:11:16 +0300

    Miss Keep Note Formatting

--- js/apps/keep/cmps/single-note.cmp.js
+++ js/apps/keep/cmps/single-note.cmp.js
@@ -3,7 +3,15 @@
 export default {
     props:['note'],
     template: `
-    <section class="note-container">{{note}}</section>
+    <section class="note-container">
+        Id:{{note.id}} <br>
+       Title: {{note.title}}<br>
+        Text: {{note.text}}<br>
+        {{note.image}}<br>
+        {{note.todos}}<br>
+        {{note.link}}<br>
+        {{note.audio}}<br>
+    </section>
     `,
     created(){
         console.log(this.note)
