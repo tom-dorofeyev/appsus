8ebd69d16727962b0cfe04953663ba160b6f8241
Nadav Abuhatsira <Nudu22@gmail.com>
2019-06-24 14:58:32 +0300

    Sorted Full Compose CPM CSS + NavBar improvements

--- js/apps/email/cmps/email-compose-full.cmp.js
+++ js/apps/email/cmps/email-compose-full.cmp.js
@@ -3,10 +3,15 @@
 import emailService from '../services/email.service.js'
 import utilService from '../../../services/utils.service.js'
 import eventBus from '../../../event-bus.js'
+import appNav from '../../../pages/app-nav.cmp.js'
 
 export default {
     template: `
         <section class="compose-container-full">
+        <img @click="toggleNav" class="apps-icon" src="img/apps.png">
+         <transition name="fade">
+             <app-nav v-if="isNavOpen"></app-nav>
+        </transition>
         <router-link to="/email">Back to Email app</router-link> |
             <section class="compose-header-full">
                 <h2 class="compose-title-full" >New Message</h2>
@@ -14,11 +19,12 @@ export default {
             <input class="compose-inputs-full" v-model="newEmail.to" placeholder="Recipients" type="text">
             <input class="compose-inputs-full" v-model="newEmail.title"  placeholder="Subject" type="text">
             <textarea class="compose-input-text-full" v-model="newEmail.text" placeholder="Body"> </textarea>
-            <router-link to="/email"><button @click="sendMail">Send</button></router-link>
+            <router-link to="/email"><button class="btn btn-primary" @click="sendMail">Send</button></router-link>
         </section>
         `,
     data() {
         return {
+            isNavOpen: false,
             newEmail: {
                 id: utilService.makeId(),
                 title: '',
@@ -39,12 +45,16 @@ export default {
         }
     },
     components: {
+        appNav,
     },
     created() {
         const emailId = this.$route.params.emailId;
         emailService.getEmailById(emailId).then(email => this.currentEmail = email);
     },
     methods: {
+        toggleNav() {
+            this.isNavOpen = !this.isNavOpen
+        },
         sendMail() {
             emailService.add(this.newEmail).then(emails => {
                     eventBus.$emit('update-emails', emails);
